16163122ffa275e951fd99227efce71483a33460
Marcio Mendes <marcio.souza@luizalabs.com>
2019-12-19 18:33:27 -0300

    chore: setting up monorepo with lerna and yarn ws

---
+++ packages/components/src/components/Button/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+  background: #0aa;
+  color: #fff;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 1rem;
+  font-weight: 300;
+  padding: 9px 36px;
+`
